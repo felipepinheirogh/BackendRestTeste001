72bb5606e28ce9e34b58332f41455e732d9c5a36
Felipe Pinheiro <88994655+felipepinheirogh@users.noreply.github.com>
2025-06-25 21:11:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const SUPABASE_URL = 'https://rigjmzuqlpzxbvsyrsqo.supabase.co';
-const SUPABASE_KEY = 'SUA_CHAVE_AQUI'; // 🔒 Substitua pela sua chave real
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZ2ptenVxbHB6eGJ2c3lyc3FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4OTAxMjksImV4cCI6MjA2NjQ2NjEyOX0.MdbKZkdinux2ZjXdXC3K-7DlJumWR2K1-u1y4maaOM0'; // 🔒 Substitua pela sua chave real
 
 async function supabaseRequest(method, endpoint, body = null) {
   const options = {
